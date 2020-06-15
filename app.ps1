b11faf96450edf8f52e22ed2de47e09b8b246448
sakai-memoru <sakai.memoru@gmail.com>
2020-06-15 10:26:48 +0900

    mod powershell batch script

--- app.ps1
+++ app.ps1
@@ -1,15 +1,17 @@
 # param (
-#     [Parameter(Mandatory=$true)][string]$excelFile = "appFieldDef.xlsm",
+#     [Parameter(Mandatory=$true)][string]$excelFile = "appOutReport.xlsm",
 #     [Parameter(Mandatory=$true)][string]$macro = "FieldDefMain.Batch",
 #     [Parameter(Mandatory=$true)][string]$formName = "FIELD_DEF",
 #     [Parameter(Mandatory=$true)][boolean]$outTemplOn = $true,
 #     [Parameter(Mandatory=$true)][boolean]$moveOn = $false
 # )
 
-$excelFile = "appReportDef.xlsm"
+$excelFile = "appReportOut.xlsm"
 $macro = "ReportDefMain.Batch"
 $formName = "REPORT_DEF"
-$outTemplOn = $true
+$outTemplOn = $false
+$outTemplHtmlOn = $false
+$dumpOn = $false
 $moveOn = $false
 ##
 $curFolder = pwd 
@@ -17,8 +19,9 @@ $fullpath = Join-Path $curFolder.Path $excelFile
 $excel = new-object -comobject excel.application
 $excel.Visible = $false
 $workbook = $excel.workbooks.open($fullpath)
-$excel.Run($macro, $formName, $outTemplOn, $moveOn)
+$null = $excel.Run($macro, $formName, $outTemplOn, $outTemplHtmlOn, $dumpOn, $moveOn)
 $workbook.close()
 $excel.Quit()
-[System.Runtime.Interopservices.Marshal]::ReleaseComObject($excel)
-Remove-Variable excel
+echo 'finish ..... !'
+$null = [System.Runtime.Interopservices.Marshal]::ReleaseComObject($excel)
+$null = Remove-Variable excel
